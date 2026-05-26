e55c297a42c3ee0f9d957ef575a2b9eeb34eb4a3
Sidharthvs4193 <134833148+Sidharthvs4193@users.noreply.github.com>
2026-05-26 09:45:50 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> cache;
+    int rob(vector<int>& nums) {
+        cache.resize(nums.size(),-1);
+        return dfs(nums,0);
+    }
+    int dfs(vector<int>& nums,int i){
+        if(i>=nums.size())return 0;
+        if(cache[i]!=-1)return cache[i];
+        return cache[i]=max(dfs(nums,i+1),nums[i]+dfs(nums,i+2));
+    }
+};
